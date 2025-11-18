ca03a8951e8df0e771478e5e1b33ebc9c8788905
Akshat Raj Saxena <119042958+akshatrajsaxena@users.noreply.github.com>
2025-11-18 21:26:13 +0530

    Update routes.py

--- foodGPT/Foodimg2Ing/routes.py
+++ foodGPT/Foodimg2Ing/routes.py
@@ -1,93 +1,68 @@
-# from flask import Flask, render_template, request, session, url_for, redirect
-# from .llm_chat import llm_chat_bp
-# import os
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
-# app.register_blueprint(llm_chat_bp)
-
-# @app.route('/',methods=['GET'])
-# def home():
-#     return render_template('home.html')
-# @app.route('/about',methods=['GET'])
-# def about():
-#     return render_template('about.html')
-# @app.route('/chat')
-# def chat_page():
-#     recipe_index = int(request.args.get('recipe_index', 0))
-#     recipe_data = session.get('recipe_data', None)
-    
-#     if not recipe_data:
-#         return redirect(url_for('home'))
-    
-#     titles = recipe_data.get('titles', ['No Recipe', 'No Recipe'])
-#     ingredients = recipe_data.get('ingredients', [[], []])
-#     recipes = recipe_data.get('recipes', [[], []])
-    
-#     selected_title = titles[recipe_index] if recipe_index < len(titles) else 'No Recipe'
-#     selected_ingredients = ingredients[recipe_index] if recipe_index < len(ingredients) else []
-#     selected_recipe = recipes[recipe_index] if recipe_index < len(recipes) else []
-    
-#     return render_template('chat.html', 
-#                           title=[selected_title],
-#                           ingredients=[selected_ingredients], 
-#                           recipe=[selected_recipe])
-    
-# @app.route('/',methods=['POST','GET'])
-# def predict():
-#     imagefile=request.files['imagefile']
-#     image_path=os.path.join(app.root_path,'./static/demo_imgs',imagefile.filename)
-#     imagefile.save(image_path)
-#     img="./static/images"+imagefile.filename
-#     title,ingredients,recipe = output(image_path)
-#     return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=img)
-
-# @app.route('/chat')
-# def chat_page():
-#     recipe_index = int(request.args.get('recipe_index', 0))
-#     recipe_data = session.get('recipe_data', None)
-    
-#     if not recipe_data:
-#         return redirect(url_for('home'))
-    
-#     titles = recipe_data.get('titles', ['No Recipe', 'No Recipe'])
-#     ingredients = recipe_data.get('ingredients', [[], []])
-#     recipes = recipe_data.get('recipes', [[], []])
-    
-#     selected_title = titles[recipe_index] if recipe_index < len(titles) else 'No Recipe'
-#     selected_ingredients = ingredients[recipe_index] if recipe_index < len(ingredients) else []
-#     selected_recipe = recipes[recipe_index] if recipe_index < len(recipes) else []
-    
-#     return render_template('chat.html', 
-#                           title=[selected_title],
-#                           ingredients=[selected_ingredients], 
-#                           recipe=[selected_recipe])
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-    
-    
-from flask import render_template ,url_for,flash,redirect,request
+from flask import render_template, url_for, flash, redirect, request, session
 from Foodimg2Ing import app
 from Foodimg2Ing.output import output
 import os
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def home():
+    # Clear any previous session data when returning to home
+    session.pop('current_recipe', None)
     return render_template('home.html')
-@app.route('/about',methods=['GET'])
+
+@app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def predict():
-    imagefile=request.files['imagefile']
-    image_path=os.path.join(app.root_path,'./static/demo_imgs',imagefile.filename)
+    imagefile = request.files['imagefile']
+    image_path = os.path.join(app.root_path, './static/demo_imgs', imagefile.filename)
     imagefile.save(image_path)
-    img="./static/images"+imagefile.filename
-    title,ingredients,recipe = output(image_path)
-    return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=img)
+    img = "./static/images" + imagefile.filename
+    title, ingredients, recipe = output(image_path)
+    
+    # Store recipe data in session for chat functionality
+    session['current_recipe'] = {
+        'title': title,
+        'ingredients': ingredients,
+        'recipe': recipe,
+        'img': img
+    }
+    session.modified = True
+    
+    return render_template('predict.html', title=title, ingredients=ingredients, recipe=recipe, img=img)
+
 @app.route('/<samplefoodname>')
 def predictsample(samplefoodname):
-    imagefile=os.path.join(app.root_path,'./static/images',str(samplefoodname)+".jpg")
-    img="/images/"+str(samplefoodname)+".jpg"
-    title,ingredients,recipe = output(imagefile)
-    return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=img)
+    imagefile = os.path.join(app.root_path, './static/images', str(samplefoodname) + ".jpg")
+    img = "/images/" + str(samplefoodname) + ".jpg"
+    title, ingredients, recipe = output(imagefile)
+    
+    # Store recipe data in session for chat functionality
+    session['current_recipe'] = {
+        'title': title,
+        'ingredients': ingredients,
+        'recipe': recipe,
+        'img': img
+    }
+    session.modified = True
+    
+    return render_template('predict.html', title=title, ingredients=ingredients, recipe=recipe, img=img)
+
+@app.route('/chat')
+def chat():
+    """Route to display chat interface"""
+    # Get recipe data from session
+    recipe_data = session.get('current_recipe', None)
+    
+    if not recipe_data:
+        # If no recipe in session, redirect to home
+        flash('Please identify a recipe first before using the chat feature.', 'warning')
+        return redirect(url_for('home'))
+    
+    # Pass recipe data to chat template
+    return render_template('chat.html', 
+                         title=recipe_data.get('title', ['']),
+                         ingredients=recipe_data.get('ingredients', [[]]),
+                         recipe=recipe_data.get('recipe', [[]]),
+                         img=recipe_data.get('img', ''))
